d3cda96cabffccd633577064891a5ae2d589f1ca
Fahd Rahali <rahali.fahd@gmail.com>
2022-05-05 12:56:50 +0100

    write action and reducer for books

---
+++ src/redux/books/books.js
@@ -0,0 +1,31 @@
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+const initialState = {
+  books: [],
+};
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (book) => ({
+  type: REMOVE_BOOK,
+  book,
+});
+
+const bookReducer = ({ state = initialState, action }) => {
+  switch (action.type) {
+    case ADD_BOOK: return [
+      ...state.books,
+      {
+        title: action.book.title,
+        author: action.book.author,
+      },
+    ];
+    case REMOVE_BOOK: return state.books.filter((book) => book.id !== action.book.id);
+    default: return state;
+  }
+};
+
+export default bookReducer;
