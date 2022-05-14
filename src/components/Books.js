ea52673ebb86160e6d0b7abb216c4226db212090
Fahd Rahali <rahali.fahd@gmail.com>
2022-05-14 07:41:15 +0100

    use useSelector to display  books from react redux

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
-  const [books, setBooks] = useState([]);
+  const books = useSelector((state) => state.books.books);
 
-  const listBooks = [{ id: 1, title: 'The Hunger Games', author: 'Suzannes Collins' },
-    { id: 2, title: 'Dune', author: 'Frank' }];
+  // const books = [{ id: 1, title: 'The Hunger Games', author: 'Suzannes Collins' },
+  //   { id: 2, title: 'Dune', author: 'Frank' }];
 
-  useEffect(() => {
-    setBooks([...listBooks]);
-  }, [setBooks]);
+  // useEffect(() => {
+  //   setBooks([...listBooks]);
+  // }, [setBooks]);
 
   return (
     <div className="books">
--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
         <Home />
       </BrowserRouter>
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
   ,
 );
